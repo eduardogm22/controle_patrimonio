ad4ed08b8532775fc2ad522001056d559b7c36c2
eduardogm22 <edu_grosselli@hotmail.com>
2024-10-23 08:59:22 -0300

    criadas e relacionadas foreign keys, iniciada criação da auditoria(logs)

--- PATP/Banco/query_main_controle_patrimonio.sql
+++ PATP/Banco/query_main_controle_patrimonio.sql
@@ -12,6 +12,42 @@ DELIMITER ;
 
 call cadastra_varios(1, 'cadeira', 5);
 
-select * from teste;
+#criando tables para chave estrangeira
+create table categorias (
+	idcategoria integer not null auto_increment primary key,
+    nome varchar(100) not null unique
+);
 
-truncate table teste;
+create table fornecedores (
+	idfornecedor integer not null auto_increment primary key,
+    nome varchar(30)
+);
+
+#alterando nomes do campo id, pois cada tabela precisa ter o seu próprio
+alter table usuario
+change id id_usuario integer not null auto_increment;
+
+alter table produto
+change id id_produto integer not null auto_increment;
+
+#inserindo a chave estrangeira das tabelas categorias e fornecedores
+
+#alterando o nome das colunas e mudando seus types pra integer
+alter table produto
+change categoria id_categoria integer not null;
+
+alter table produto
+change fornecedor id_fornecedor integer not null;
+
+#adicionando a chave estrangeira nas tabelas
+alter table produto
+add constraint fk_prd_categorias foreign key (id_categoria) references categorias (id_categoria);
+
+alter table produto
+add constraint fk_prd_fornecedores foreign key (id_fornecedor) references fornecedores (id_fornecedor);
+
+#criando tabela para fazer a auditoria(logs) da tabela produtos
+
+create table produtos_audit(
+	
+);
