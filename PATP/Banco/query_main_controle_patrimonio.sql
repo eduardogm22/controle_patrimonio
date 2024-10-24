3ad968e0ae25186a7f3d4dff747ecc5edb85c11b
eduardogm22 <edu_grosselli@hotmail.com>
2024-10-24 13:31:17 -0300

    continuando a parte de criação das foreign keys

--- PATP/Banco/query_main_controle_patrimonio.sql
+++ PATP/Banco/query_main_controle_patrimonio.sql
@@ -1,4 +1,4 @@
-
+﻿
 use cadastro;
 #Procedure criada (como modelo) para cadastrar varios produtos diferentes com base na quantidade
 DELIMITER $$
@@ -29,12 +29,16 @@ create table fornecedores (
 
 #alterando nomes do campo id, pois cada tabela precisa ter o seu próprio
 alter table usuario
-change id id_usuario integer not null auto_increment;
+change id id_usuario integer auto_increment;
 
 alter table produto
-change id id_produto integer not null auto_increment;
+change id id_produto integer auto_increment;
 
-#inserindo a chave estrangeira das tabelas categorias e fornecedores
+alter table pessoas
+change id id_pessoa integer auto_increment;
+
+alter table cargo
+change id id_cargo integer auto_increment;
 
 #alterando o nome das colunas e mudando seus types pra integer
 alter table produto
@@ -43,6 +47,9 @@ change categoria id_categoria integer not null;
 alter table produto
 change fornecedor id_fornecedor integer not null;
 
+alter table pessoas
+change cargo id_cargo integer not null default 2; ###verificar pois tem valores null
+
 #adicionando a chave estrangeira nas tabelas
 alter table produto
 add constraint fk_prd_categorias foreign key (id_categoria) references categorias (id_categoria);
@@ -50,8 +57,18 @@ add constraint fk_prd_categorias foreign key (id_categoria) references categoria
 alter table produto
 add constraint fk_prd_fornecedores foreign key (id_fornecedor) references fornecedores (id_fornecedor);
 
+alter table pessoas
+add constraint fk_pes_cargo foreign key (idcargo) references cargo (idcargo);
+
 #criando tabela para fazer a auditoria(logs) da tabela produtos
 
 #create table produtos_audit();
 
 select * from teste_produtos;
+
+select * from `pessoas`;
+
+#1 administrador 7
+#2 funcionario leitor
+#3 funcionario controle
+#4 supervisor
