8be5d2dfe65d8a8b4c68985783e28807e7a25e23
eduardogm22 <edu_grosselli@hotmail.com>
2024-10-25 08:58:19 -0300

    iniciando ajustes no banco de dados

---
+++ PATP/Banco/criacao_tables.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE produtos (
+ id_produto INTEGER PRIMARY KEY AUTO_INCREMENT,
+ nome VARCHAR(100) NOT NULL,
+ valor_unitario FLOAT NOT NULL,
+ numero_patrimonio VARCHAR(30) UNIQUE,
+ idinfo_nota INTEGER (FK),
+ idcategoria INTEGER NOT NULL (FK),
+ idsetor_responsavel INTEGER NOT NULL (FK),
+ idsituacao INTEGER NOT NULL (FK),
+ idfornecedor INTEGER NOT NULL (FK)
+
+
+); 
